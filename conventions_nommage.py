d708da093cdcf98dacea871ba055c876e8e8a941
Ulrich75 <ulrichhoungbe75@gmail.com>
2024-05-03 17:30:45 +0100

    ajout du fichier conventions de nommage

---
+++ conventions_nommage.py
@@ -0,0 +1,36 @@
+# SNAKE_CASE : variable en miniscule
+
+name = "jeannne"
+fuel_level = 100
+
+
+# LES VARIABLES CONSTANTES : variable en majuscule avec underscores
+
+DAYS_PER_WEEK = 7
+PERSONAL_EMAIL = "examplemail@email.com"
+
+# CAPITALIZEDCASE : il s'agit du nom des classe 
+# Debut de chaque mot en Majuscule sans ponctuation
+
+class JukeBox:
+    pass
+
+class SpaceShip:
+    pass
+
+# NOM DES MODULES : Tout en minuscule sans underscores
+
+import os
+import numpy
+
+# Mettez les commentaires à jour lorsque le code change.
+
+# Évitez les commentaires sur la même ligne que le code, ils font négligé.
+
+# Mettez un seul espace entre le symbole dièse et le texte (pour différencier du code qui a été commenté).
+
+# Utilisez le même niveau d’indentation que la ligne de code qui suit, pour une bonne lisibilité
+
+
+
+# Mettez les docstrings et les commentaire en anglais
